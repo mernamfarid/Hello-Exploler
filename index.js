da05902282ad3b485bdb9366feed5b90cda96211
mernamfarid <111951763+mernamfarid@users.noreply.github.com>
2023-04-13 23:54:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let usersSection = document.querySelector(".haidshow")
+let isShow =true;
+function showHideUsers(){
+    if (isShow){ 
+        userSection.style.display= "none"; 
+        isShow =false;
+}    else{
+    usersSection.style.display= "bloke"; 
+    isShow =true;
+
+}
+   
+}
+
+// function showHideUsers(){
+//     userSection.classlist.toggle("hide")
+// }
